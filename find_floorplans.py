31ee4aa2b5a4a5163237ed385bb409325a5bf462
Nathaniel Taulbut <ntaulbut@gmail.com>
2024-07-09 21:10:14 +0100

    more sites and get lower ground

--- find_floorplans.py
+++ find_floorplans.py
@@ -2,23 +2,41 @@ import json
 import requests
 
 BASE_URL = "https://www.nottingham.ac.uk/estates/documents/accessplans/{}{}a.pdf"
-NOT_FOUND = 404
 SUCCESS = 200
+A_ORD = 97
+Z_ORD = 123
+LOWER_GROUND_FLOOR = "lg"
+
+
+def alpha_range():
+    for n in range(A_ORD, Z_ORD + 1):
+        yield chr(n)
+
 
 with open("buildings.json") as f:
     campuses = json.load(f)
 
-floor_plans = {campus: {building: {} for building in buildings} for campus, buildings in campuses.items()}
+floor_plans = {
+    campus: {building: {} for building in buildings}
+    for campus, buildings in campuses.items()
+}
 
 for campus, buildings in campuses.items():
     for building_name, building_code in buildings.items():
-        for i in range(97, 123):
-            floor_name = chr(i)
-            url = BASE_URL.format(building_code, floor_name)
-            r = requests.get(url)
+        r = requests.get(BASE_URL.format(building_code, LOWER_GROUND_FLOOR))
+        if r.status_code == SUCCESS:
+            print(
+                f"{campus}: Added lower ground floor plan to '{building_name}': {r.url}"
+            )
+            floor_plans[campus][building_name][LOWER_GROUND_FLOOR] = r.url
+
+        for floor_name in alpha_range():
+            r = requests.get(BASE_URL.format(building_code, floor_name))
             if r.status_code == SUCCESS:
-                print(f"{campus}: Added floor plan to '{building_name}': {floor_name}: {url}")
-                floor_plans[campus][building_name][floor_name] = url
+                print(
+                    f"{campus}: Added floor plan to '{building_name}': {floor_name}: {r.url}"
+                )
+                floor_plans[campus][building_name][floor_name] = r.url
             else:
                 break
 
